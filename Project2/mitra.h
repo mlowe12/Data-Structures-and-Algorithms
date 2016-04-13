6a9699154df92cd21b270932fc97c3fbafadf1dd
Quinton Reddick <qreddick@ufl.edu>
2016-04-13 02:14:19 -0400

    Classes are created

---
+++ Project2/mitra.h
@@ -0,0 +1,19 @@
+#ifndef MITRA_H_1234
+#define MITRA_H_1234
+
+#include <string>
+#include <iostream>
+#include <vectors>
+#include <iterator>
+
+using namespace std;
+
+class Magi {
+  
+};
+
+class Realm {
+
+};
+
+#endif
