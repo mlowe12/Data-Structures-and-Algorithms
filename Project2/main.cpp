49baa6874a8a43d2c639a6b0d96b13270a0a136b
mlowe12 <mlowe_12@hotmail.com>
2016-04-21 23:20:30 -0400

    Update main.cpp

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,28 +1,44 @@
-#include <mitra.h>
+#include <iostream> 
+#include <vector> 
+#include <list> 
+#include <algorithm>
+#include "mitra.h" 
+#include <string>
 
-using namespace std;
 
-int main() {
-	int numberofRealms;
-	string charm;
-	int magiCount;
-	string magiOrder;
+int main(int argc, char** argv)
+{
+	int numberOfRealms; 
+	std::string charm; 
+	int magiCount; 
+	std::string magiOrder; 
+	int numberOfCharms = 1; //the charm can change, but only one can exist at a time. 
+		
+	std::cin>>numberOfRealms; // recieve number of realms
+ 
 	
-	for (int i= 0; i < numberofRealms; i++) {
-		// Create the number of Realms - Initialize
-
-		// get the charm Name
-
-
-		// get the number of Magi in the realm
-
-		// get the order of the magi
+	/*iterate for the  number of realms and attributes of input stream*/ 
+	for(int i =0; i < numberOfRealms; ++i)
+	{ 
+		 
+		/*start to recieve arguments*/ 
+ 
+		std::getline(std::cin, charm); //recieves name of charm 
+		int numberOfMagi; 	
+		std::cin >> numberOfMagi; //recieves number of Magi in realm 
+		std::getline(std::cin, magiOrder);  // recieves array of Magi as a string *parse later*
 	}
-
-	// find the Longest Increasing Subsequence of the Magi Order
-	// that sequence indicated how many changes you can make to the Charm Name
-
-	// function for Edit distance between one realm and all the others - Do this for all realms
 	
-	
+	/* argument recpetion complete*/ 
+ 
+	/*quinton*/
+	//find the Longest Increasing Subsequence of the Magi Order
+	/*joe or bradon?*/
+	//that sequence indicated how many changes you can make the Charm Name
+
+	/*working on this*/
+	//function for Edit distance between one realm and all the others -- Do this for all realms
+
+		return 0; 
 }
+
